829c7b83d3fd8314c35aa7daac134807b7505ee0
SametYASIT <120249031+SametYASIT@users.noreply.github.com>
2024-11-21 11:19:02 +0300

    table düzenlendi

--- yokVeriAnalizi/resources/views/grid.blade.php
+++ yokVeriAnalizi/resources/views/grid.blade.php
@@ -242,79 +242,59 @@
         <!-- ============================================================== -->
         <!-- Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
-        <aside class="left-sidebar" data-sidebarbg="skin5">
-            <!-- Sidebar scroll-->
-            <div class="scroll-sidebar">
-                <!-- Sidebar navigation-->
-                <nav class="sidebar-nav">
-                    <ul id="sidebarnav" class="pt-4">
-                        <li class="sidebar-item">
-                            <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                               href="{{ url('/index') }}" aria-expanded="false">
-                                <i class="mdi mdi-view-dashboard"></i>
-                                <span class="hide-menu">Dashboard</span>
-                            </a>
-                        </li>
+       <aside class="left-sidebar" data-sidebarbg="skin5">
+           <!-- Sidebar scroll-->
+           <div class="scroll-sidebar">
+               <!-- Sidebar navigation-->
+               <nav class="sidebar-nav">
+                   <ul id="sidebarnav" class="pt-4">
+                       <li class="sidebar-item">
+                           <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                              href="{{ url('/index') }}" aria-expanded="false">
+                               <i class="mdi mdi-view-dashboard"></i>
+                               <span class="hide-menu">Dashboard</span>
+                           </a>
+                       </li>
 
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                                     href="{{ url('/charts') }}" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span
-                                    class="hide-menu">Charts</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                                     href="{{ url('/widgets') }}" aria-expanded="false"><i class="mdi mdi-chart-bubble"></i><span
-                                    class="hide-menu">Widgets</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                                     href="{{ url('/tables') }}" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span
-                                    class="hide-menu">Tables</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                                     href="{{ url('/grid') }}" aria-expanded="false"><i class="mdi mdi-blur-linear"></i><span
-                                    class="hide-menu">Full Width</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark"
-                                                     href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span
-                                    class="hide-menu">Forms </span></a>
-                            <ul aria-expanded="false" class="collapse  first-level">
-                                <li class="sidebar-item"><a href="{{ url('/form-basic') }}" class="sidebar-link"><i
-                                            class="mdi mdi-note-outline"></i><span class="hide-menu"> Form Basic
-                                        </span></a></li>
-                                <li class="sidebar-item"><a href="{{ url('/form-wizard') }}" class="sidebar-link"><i
-                                            class="mdi mdi-note-plus"></i><span class="hide-menu"> Form Wizard
-                                        </span></a></li>
-                            </ul>
-                        </li>
+                       <li class="sidebar-item">
+                           <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                              href="{{ url('/charts') }}" aria-expanded="false">
+                               <i class="mdi mdi-chart-bar"></i>
+                               <span class="hide-menu">Charts</span>
+                           </a>
+                       </li>
 
-                        <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark"
-                                                     href="javascript:void(0)" aria-expanded="false"><i
-                                    class="mdi mdi-move-resize-variant"></i><span class="hide-menu">Addons </span></a>
-                            <ul aria-expanded="false" class="collapse  first-level">
-                                <li class="sidebar-item"><a href="{{ url('/pages-gallery') }}" class="sidebar-link"><i
-                                            class="mdi mdi-multiplication-box"></i><span class="hide-menu"> Gallery
-                                        </span></a></li>
-                                <li class="sidebar-item"><a href="{{ url('/pages-calendar') }}" class="sidebar-link"><i
-                                            class="mdi mdi-calendar-check"></i><span class="hide-menu"> Calendar
-                                        </span></a></li>
-                                <li class="sidebar-item"><a href="{{ url('/pages-invoice') }}" class="sidebar-link"><i
-                                            class="mdi mdi-bulletin-board"></i><span class="hide-menu"> Invoice
-                                        </span></a></li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark"
-                                                     href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-account-key"></i><span
-                                    class="hide-menu">Authentication </span></a>
-                            <ul aria-expanded="false" class="collapse  first-level">
-                                <li class="sidebar-item"><a href="{{ url('/authentication-login') }}" class="sidebar-link"><i
-                                            class="mdi mdi-all-inclusive"></i><span class="hide-menu"> Login </span></a>
-                                </li>
-                                <li class="sidebar-item"><a href="{{ url('/authentication-register') }}" class="sidebar-link"><i
-                                            class="mdi mdi-all-inclusive"></i><span class="hide-menu"> Register
-                                        </span></a></li>
-                            </ul>
-                        </li>
+                       <li class="sidebar-item">
+                           <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                              href="{{ url('/widgets') }}" aria-expanded="false">
+                               <i class="mdi mdi-chart-bubble"></i>
+                               <span class="hide-menu">New Updates</span>
+                           </a>
+                       </li>
 
-                    </ul>
-                </nav>
-                <!-- End Sidebar navigation -->
-            </div>
-            <!-- End Sidebar scroll-->
-        </aside>
+                       <li class="sidebar-item">
+                           <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                              href="{{ url('/tables') }}" aria-expanded="false">
+                               <i class="mdi mdi-border-inside"></i>
+                               <span class="hide-menu">Tables</span>
+                           </a>
+                       </li>
+
+                       <li class="sidebar-item">
+                           <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                              href="{{ url('/pages-gallery') }}" aria-expanded="false">
+                               <i class="mdi mdi-multiplication-box"></i>
+                               <span class="hide-menu">Gallery</span>
+                           </a>
+                       </li>
+                   </ul>
+               </nav>
+               <!-- End Sidebar navigation -->
+           </div>
+           <!-- End Sidebar scroll-->
+       </aside>
+
+
         <!-- ============================================================== -->
         <!-- End Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
