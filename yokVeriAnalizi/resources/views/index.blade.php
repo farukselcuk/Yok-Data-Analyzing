f3efbea59e658a24b9aa18ccb339bbb2fc98e7d2
SametYASIT <120249031+SametYASIT@users.noreply.github.com>
2024-11-21 12:38:34 +0300

    cizelgeler düzenlendi

--- yokVeriAnalizi/resources/views/index.blade.php
+++ yokVeriAnalizi/resources/views/index.blade.php
@@ -257,7 +257,7 @@
                             <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                href="{{ url('/index') }}" aria-expanded="false">
                                 <i class="mdi mdi-view-dashboard"></i>
-                                <span class="hide-menu">Dashboard</span>
+                                <span class="hide-menu">Kontol paneli</span>
                             </a>
                         </li>
 
@@ -265,7 +265,7 @@
                             <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                href="{{ url('/charts') }}" aria-expanded="false">
                                 <i class="mdi mdi-chart-bar"></i>
-                                <span class="hide-menu">Charts</span>
+                                <span class="hide-menu">Cizelgeler</span>
                             </a>
                         </li>
 
@@ -273,7 +273,7 @@
                             <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                href="{{ url('/widgets') }}" aria-expanded="false">
                                 <i class="mdi mdi-chart-bubble"></i>
-                                <span class="hide-menu">New Updates</span>
+                                <span class="hide-menu">Yeni Güncellemeler</span>
                             </a>
                         </li>
 
@@ -281,7 +281,7 @@
                             <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                href="{{ url('/tables') }}" aria-expanded="false">
                                 <i class="mdi mdi-border-inside"></i>
-                                <span class="hide-menu">Tables</span>
+                                <span class="hide-menu">Tablolar</span>
                             </a>
                         </li>
 
@@ -289,7 +289,7 @@
                             <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                href="{{ url('/pages-gallery') }}" aria-expanded="false">
                                 <i class="mdi mdi-multiplication-box"></i>
-                                <span class="hide-menu">Gallery</span>
+                                <span class="hide-menu">Galeri</span>
                             </a>
                         </li>
                     </ul>
